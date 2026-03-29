6eae43546981e16c895addba402b8f8a7be8a03b
Kadir <kadir.cinek@hotmail.com>
2026-03-29 14:14:42 +0300

    Eski URL'ler icin 301 redirect eklendi

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,26 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/tr/buteo-petrochemicals",
+        destination: "/tr",
+        permanent: true,
+      },
+      {
+        source: "/en/buteo-petrochemicals",
+        destination: "/en",
+        permanent: true,
+      },
+      {
+        source: "/buteo-petrochemicals",
+        destination: "/tr",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
